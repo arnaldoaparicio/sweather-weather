1d3911b1c4dd0a99d0a57013bbd522743fd7dff3
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-03-07 13:54:12 -0500

    Fix indentation

--- app/facades/books_facade.rb
+++ app/facades/books_facade.rb
@@ -1,5 +1,10 @@
 class BooksFacade
 
-  def
+  def self.get_books(city, quantity)
+    books = BooksService.book_search(city, quantity)[:docs]
 
+    books.map do |book|
+      Book.new(book)
+    end
+  end
 end
