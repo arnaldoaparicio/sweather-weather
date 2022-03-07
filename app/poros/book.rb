dfb0dc3e285195c79a4dc0779b84ce30de239f27
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-03-07 13:50:22 -0500

    Add isbn, title, and publisher attributes

---
+++ app/poros/book.rb
@@ -0,0 +1,9 @@
+class Book
+  attr_reader :isbn, :title, :publisher
+  def initialize(attributes)
+    @isbn = attributes[:isbn]
+    @title = attributes[:title]
+    @publisher = attributes[:publisher]
+
+  end
+end
