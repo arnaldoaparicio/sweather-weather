eedb17697daf9aaceaf06cb011f094559729c545
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-03-07 11:47:23 -0500

    Comment out test

--- spec/requests/api/v1/background_request_spec.rb
+++ spec/requests/api/v1/background_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Background API' do
-  it 'gets the background' do
+  xit 'gets the background' do
     get '/api/v1/backgrounds?location=denver,co'
 
     expect(response).to be_successful
