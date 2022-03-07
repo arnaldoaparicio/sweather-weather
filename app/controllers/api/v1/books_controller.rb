ea470d385b3e9a0cdc52aa824ced5ef325408c3c
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-03-07 13:53:04 -0500

    Complete Book Controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::BooksController < ApplicationController
   def index
-  end
-
+    books = BooksFacade.get_books(params[:location], params[:quantity])
+    city_and_state = params[:location].split(',')
+    coordinates = CoordinatesFacade.get_coordinates(city_and_state[0], city_and_state[1])
+    forecast = ForecastFacade.current_weather(coordinates.latitude, coordinates.longitude)
+    book_count = BooksService.book_search(params[:location], params[:quantity])
+    render json: BooksSerializer.books_forecast(forecast, books, book_count, city_and_state)
+  end  
 end
