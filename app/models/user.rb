e18916defaef98e554f6e4cc770b70acb20ec99a
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-03-08 13:02:47 -0500

    Change behavior of has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates_presence_of :email
   validates_uniqueness_of :email, case_sensitive: false
 
-  has_secure_password(validations: false)
+  has_secure_password(validations: true)
+  has_secure_token :api_key
 
 end
