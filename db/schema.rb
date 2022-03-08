a2082988ac96e189e323d354e28d50775ade4e73
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-03-08 13:05:04 -0500

    Change schema

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,9 @@ ActiveRecord::Schema.define(version: 2022_03_07_212531) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
