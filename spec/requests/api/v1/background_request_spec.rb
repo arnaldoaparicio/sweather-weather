e2dd41874931b703bfa4ee0977b156ce437724b2
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-03-08 13:04:37 -0500

    Fix test

--- spec/requests/api/v1/background_request_spec.rb
+++ spec/requests/api/v1/background_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Background API' do
-  it 'gets the background' do
+  it 'gets the background', :vcr do
     get '/api/v1/backgrounds?location=denver,co'
 
     expect(response).to be_successful
@@ -9,6 +9,10 @@ RSpec.describe 'Background API' do
 
     background = JSON.parse(response.body, symbolize_names: true)
 
-    expect(backgrounds).to be_a(Hash)
+    expect(background).to be_a(Hash)
+    expect(background).to have_key :data
+
+    expect(background[:data]).to have_key :id
+    expect(background[:data][:id]).to eq('null')
   end
 end
