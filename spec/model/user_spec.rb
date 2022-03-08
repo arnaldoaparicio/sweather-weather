bdd6a6af92ee089fd9060c367c746c84ef6f5775
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-03-08 13:03:49 -0500

    Add more tests

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email).case_insensitive }
   end
+
+  it 'encrypts password' do
+    test_user = User.create(email: 'example@mail.com', password: 'test123', password_confirmation: 'test123')
+    expect(test_user).to_not have_attribute(:password)
+    expect(test_user).to_not have_attribute(:password_confirmation)
+    expect(test_user.password_digest).to_not eq('test123')
+  end
 end
