0c23246dd5ba34b5bec5bbf394ba5d1fe82da547
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-07-13 22:19:52 -0400

    Add test for daily_weather method

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -1,12 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe ForecastFacade do
-  it 'describes the current weather forecast' do
+  it 'describes the current weather forecast', :vcr do
     # Coordinates are from Denver, CO
     current_weather = ForecastFacade.current_weather(39.738453, -104.984853)
 
     expect(current_weather).to be_a CurrentWeather
   end
 
-  it ''
+  it 'describes the daily weather for five days', :vcr do
+
+    daily_weather = ForecastFacade.daily_weather(39.738453, -104.984853)
+
+    expect(daily_weather).to be_an Array
+  end
 end
