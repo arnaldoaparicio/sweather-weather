4fcfb5bc2544f76ecffc0e810795ec8bcaab5c81
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-07-13 22:04:34 -0400

    Add test for current_weather method

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -1,1 +1,12 @@
+require 'rails_helper'
 
+RSpec.describe ForecastFacade do
+  it 'describes the current weather forecast' do
+    # Coordinates are from Denver, CO
+    current_weather = ForecastFacade.current_weather(39.738453, -104.984853)
+
+    expect(current_weather).to be_a CurrentWeather
+  end
+
+  it ''
+end
