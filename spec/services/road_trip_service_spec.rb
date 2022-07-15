032f84889bbd45935303e9f9809baf23cac8eef1
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-07-14 21:49:47 -0400

    Fix spacing

--- spec/services/road_trip_service_spec.rb
+++ spec/services/road_trip_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Road Trip API' do
    it 'it returns directions', :vcr do
      roadtrip = RoadTripService.roadtrip_directions('Denver, CO', 'Estes Park, CO')
-
+    
      expect(roadtrip).to be_a Hash
    end
 end
