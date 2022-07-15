e8ff60ff449339a47a75e7e4f566cce997a1627c
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-07-14 21:50:12 -0400

    Add test to show it exists

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -6,8 +6,21 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of(:email).case_insensitive }
   end
 
+  it 'exists' do
+    data = { email: 'sample@email.com',
+             password: 'blahblah',
+             password_confirmation: 'blahblah' }
+
+    user = User.new(data)
+
+    expect(user).to be_an_instance_of(User)
+    expect(user.email).to eq('sample@email.com')
+    expect(user).to have_attribute(:password_digest)
+  end
+
   it 'encrypts password' do
     test_user = User.create(email: 'example@mail.com', password: 'test123', password_confirmation: 'test123')
+
     expect(test_user).to_not have_attribute(:password)
     expect(test_user).to_not have_attribute(:password_confirmation)
     expect(test_user.password_digest).to_not eq('test123')
